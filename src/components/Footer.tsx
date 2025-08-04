2fbe4a14f2b680ffe02781374a27e80ed984b5bd
Radexman <radoslaw.siek-web.dev@wp.pl>
2025-08-04 14:06:28 +0200

    Footer & Metadata

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import FizziLogo from './FizziLogo';
+import CircleText from './CircleText';
+
+const Footer = () => {
+  return (
+    <footer className="bg-[#fee832] text-[#f36334]">
+      <div className="relative mx-auto flex w-full max-w-4xl justify-center px-4 py-10">
+        <FizziLogo />
+        <div className="absolute top-0 right-24 size-28 origin-center -translate-y-14 md:size-48 md:-translate-y-28">
+          <CircleText />
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
