be65d36f06ef8e89313b449dc04f8dd9cb724bfc
Radexman <radoslaw.siek-web.dev@wp.pl>
2025-08-04 14:18:16 +0200

    Bounded fix

--- src/components/Bounded.tsx
+++ src/components/Bounded.tsx
@@ -6,21 +6,17 @@ type BoundedProps = {
   children: React.ReactNode;
 };
 
-const Bounded = ({
-  as: Comp = 'section',
+export const Bounded = ({
   className,
   children,
   ...restProps
 }: BoundedProps) => {
   return (
-    <Comp
-      className={clsx('px-4 first:pt-10 md:px-6', className)}
-      {...restProps}
-    >
+    <div className={clsx('px-4 first:pt-10 md:px-6', className)} {...restProps}>
       <div className="mx-auto flex w-full max-w-7xl flex-col items-center">
         {children}
       </div>
-    </Comp>
+    </div>
   );
 };
 
